486d0383befc88dfe16fb9867403c717c1fc6fb6
Miguew233 <miguelzin375@gmail.com>
2025-10-06 11:51:26 -0300

    tentei fazer

---
+++ lib/banco/restaurante_dao.dart
@@ -0,0 +1,16 @@
+import 'database_helper.dart';
+import 'package:voupedir/restaurante.dart';
+
+class RestauranteDAO{
+  static Future<List<Restaurante>> listarTodos() async{
+    final db = await DatabaseHelper.getDatabase();
+    final resultado = await db.query('tb_restaurante');
+
+    return resultado.map((mapa){
+      return Restaurante(
+      codigo: mapa['cd_restaurante'] as int,
+        nome: mapa['nm_restaurante'] as String
+      );
+    }).toList();
+  }
+}
--- lib/telas/tela_home.dart
+++ lib/telas/tela_home.dart
@@ -1,38 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:voupedir/restaurante.dart';
 import 'package:voupedir/telas/tela_cad_restaurante.dart';
+import '../banco/restaurante_dao.dart';
 
-class TelaHome extends StatelessWidget{
+class TelaHome extends StatefulWidget {
   TelaHome({super.key});
 
   @override
+  State<TelaHome> createState() => TelaHomeState();
+}
+
+Future <void> carregaRestaurante() async{
+  final lista = await RestauranteDAO.listarTodos();
+  setSate(() {
+    restaurantes = lista;
+  });
+}
+
+class TelaHomeState extends State<TelaHome>{
+  List<Restaurante> restaurantes = [];
+
+  @override
+  void initState(){
+    super.initState();
+    carregaRestaurante();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Seja bem vindo Usuário:")),
-      body: Padding(padding: const EdgeInsets.all(30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Lista de Restaurantes:"),
-            ElevatedButton(onPressed: (){
+      appBar: AppBar(
+          title: const Text("Lista de restaurantes"),
+          actions: [
+            IconButton(onPressed: (){
               Navigator.push(context, MaterialPageRoute(builder: (context) => TelaCadRestaurante()));
-            }, child: Text("Cadastrar Novo Restaurante")),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Text("Meu restaurante"),
-                     ],
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(onPressed: (){}, child: Text("Excluir")),
-                SizedBox(width: 10),
-                ElevatedButton(onPressed: (){}, child: Text("Editar"))
-              ],
-            )
+            }, icon: Icon(Icons.add))
           ],
+      ),
+      body: Padding(padding: const EdgeInsets.all(30),
+        child: ListView.builder(
+          itemCount: restaurantes.length,
+          itemBuilder: (context, index){
+            return Card(
+              margin: EdgeInsets.symmetric(vertical: 8),
+              child: ListTile(
+                title: Text('Sem nome'),
+                subtitle: Text('ID: ${r.codigo}'),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                        onPressed: (){
+                          //Codigo para Editar Restaurante
+                        },
+                        icon: Icon(Icons.edit, color: Colors.blue)
+                    ),
+                    IconButton(
+                        onPressed: (){
+                          AlertDialog(
+                            title: Text('Confirmar ação'),
+                            content: Text('Deseja realmente excluir ?'),
+                            actions: <Widget>[
+                              TextButton(
+                                  onPressed: (){
+                                    //Código para excluir o registro
+                                  },
+                                  child: Text('sim')
+                              ),
+                              TextButton(onPressed: (){
+                                //
+                              },
+                                  child: Text('não')
+                              )
+                            ],
+                          );
+                        },
+                        icon: Icon(Icons.delete, color: Colors.red)
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => TelaCadRestaurante()));
+          },
+        child: Icon(Icons.add),
       ),
     );
   }
+
 }
